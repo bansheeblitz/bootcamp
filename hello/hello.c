d8b77aeaaaf3f19ea8c64f4dd25dbcd0baadf6eb
admin <admin@localhost.localdomain>
2017-06-20 18:40:29 -0700

    new comment

---
+++ hello/hello.c
@@ -0,0 +1,34 @@
+#include <stdio.h> 
+
+int main () {
+	//float pi = 3.14159;
+	//int ipi = (int) pi;
+ 	//printf("hello, world\nThe change\n");
+	//printf("Float Pie = %f and the Int is %d \n", pi, ipi);
+	//printf("4/3 = %f\n4\%3 = %f\n4.0/3 = %f", (float)(4/3),(float)(4%3), (float)(4.0/3));
+	//printf("False = %d \n", (3==2));
+	//char a = a;
+	//char d = a;
+	//char b = a;
+	//printf("False = %d \n", (d==b));	
+	boolean((3>2)); //true
+	boolean((3<2)); //false
+	boolean((3-(2+1))); //false
+	char d = d;	 
+	boolean('d'); 	//true
+	boolean((9%3));	//true
+	printf("'d' = %d\n", 'd');
+	boolean(((3>2)-1)); //false
+	return 0;
+}
+boolean(int x) {
+	if (x == 0) {
+		printf("False\n");	
+	}
+	else {
+		printf("True\n");
+	}
+
+}
+
+
