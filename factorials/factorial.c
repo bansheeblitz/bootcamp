4dc353f4aa1a7bfd1bf63ea31b3a5c95d90c256b
bansheeblitz <bansheeblitz@hotmail.com>
2017-06-24 15:00:15 -0700

    A1, A2

--- factorials/factorial.c
+++ factorials/factorial.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
  
-int factorial(int val);
+long long int factorial(int val);
  
 int main() {
     int i;
     printf("\tValue\tFactorial\n");
     for (i = 0; i <= 20; i++) {
-        printf("\t%d\t%i\n", i, factorial(i));
+        printf("\t%d\t%lld\n", i, factorial(i));
     }
     return 0;
 }
  
 // create factorial function here:
+long long int factorial (int i) {
+   long long int n; //step
+   long long int j; //result
+   j = i;
+   while (i != 0) {
+      if ((i-1) != 0) {
+         n = j * (i-1);
+         j = n;
+      }
+      i--;
+   }
+   return j;
+}
--- freqparse/freqparse.c
+++ freqparse/freqparse.c
@@ -4,21 +4,40 @@ int main() {
 digits, 
 lower-case letters,
 other  
-
 */
   // initialize variables;
 
  
   char c;
+  int i;
+  int ndigit;
+  int loC;
+  int upC;
+  int nother;
  
   while ((c = getchar()) != EOF) {
     // process one character
-    if (c >= '0')
+    if (c >= '0' && c <= '9') {
+      ++ndigit
+    }
+    else if (islower(c)) {
+      loC++;
+    }
+    else if (isupper(c)) {
+      upC++;
+    }
+    else if (c == '' || c == '\n' || c == '\t');
+      continue;
+    }
+    else {
+      nother++;
+    }
   }
  
   // Output results here
-  printf("digits =");
-	for (i = 0; i < 10; ++i)
-	printf(" %d", ndigit[i]);   
+  printf("digits = %d\n", ndigit);
+  printf("lowercase = %d\n", loC;);
+  printf("uppercase = %d\n", upC);
+  printf("other = %d\n", nother);
   return 0;
 }
